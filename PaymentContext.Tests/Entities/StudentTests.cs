1278cbfc27899b482f66cf2aba97bd359ddaca14
Raul Silva <raul.silva@uppertools.com.br>
2020-09-16 15:05:58 -0300

    Criando a entidade StudentTests, para futuros testes

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests{
+    [TestClass]
+    public class StudentTest{
+        [TestMethod]
+        public void AdicionarAssinatura(){
+            // var subscription = new Subscription(null);
+            // var student = new Student("Raul","Silva","123123123","ryanraul_11@hotmail.com");
+            // student.AddSubscription(subscription);
+
+            
+        }
+
+
+    }
+}
