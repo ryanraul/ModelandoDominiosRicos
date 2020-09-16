5032534b56896611d3fb3eeacbdfcf7367fa254b
Raul Silva <raul.silva@uppertools.com.br>
2020-09-16 10:54:08 -0300

    Criação das Entidades

---
+++ PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace PaymentContext.Domain.Entities{
+    public class Student{
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public List<Subscription> Subscriptions { get; set; }
+    }
+}
