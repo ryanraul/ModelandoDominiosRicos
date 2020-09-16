76a013810c18eb0f665562e91abee7a8ee5e4a2a
Raul Silva <raul.silva@uppertools.com.br>
2020-09-16 11:46:33 -0300

    Estabelecendo regra de cancelamento de antigas assinaturas, e estabelecendo uma como principal

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -1,17 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PaymentContext.Domain.Entities{
     public class Student{
+
+        private IList<Subscription> _subscriptions;
         public Student(string firsName, string lastName, string document, string email)
         {
             FirsName = firsName;
             LastName = lastName;
             Document = document;
             Email = email;
+            _subscriptions = new List<Subscription>();
 
-            if(FirsName.Length == 0)
-                throw new Exception("Nome Invalido");
         }
 
         public string FirsName { get; private set; }
@@ -19,6 +21,17 @@ namespace PaymentContext.Domain.Entities{
         public string Document { get; private set; }
         public string Email { get; private set; }
         public string Address { get; private set; }
-        public List<Subscription> Subscriptions { get; private set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
+
+        public void AddSubscription(Subscription subscription){
+            // Se tiver uma assinatura ativa, cancela
+
+            // Cancela todas as assinaturas, e coloca esta como principal
+            foreach(var sub in Subscriptions){
+                sub.Active = false;
+            }
+
+            _subscriptions.Add(subscription);
+        }
     }
 }
