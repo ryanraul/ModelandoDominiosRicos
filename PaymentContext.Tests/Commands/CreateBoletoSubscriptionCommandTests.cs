a196218b88729733df9f88c7134f87d25b02943a
ryanraul <ryanraul_11@hotmail.com>
2020-09-18 09:23:35 -0300

    Criando teste para o Command da assinatura por boleto

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,26 @@
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests{
+        // Red, Green, Refactor
+        // 1- Fazer os testes falharem
+        // 2- Fazer os testes passarem
+        // 3- Refatorar o código
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid(){
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirsName = "";
+            
+            command.Validate();
+            Assert.AreEqual(false, command.Valid);
+        }
+
+
+    }
+}
